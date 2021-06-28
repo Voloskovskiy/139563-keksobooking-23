974fd2d4149e8f43da17ca04b110da947fef6921
Artem Voloskov <artem@voloskov.ru>
2021-06-28 22:32:24 +0300

    update random function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 //Первоисточник
 let generateRandomNumber = function(from, to){
-    return Math.floor(+from + Math.random() * (+to + 1 - (+from)));
+    from = Math.abs(from);
+    to = Math.abs(to);
+    if(from>=to){
+        let tmp = to;
+        to = from;
+        from = tmp;
+    }
+    return Math.floor(from + Math.random() * (to + 1 - from));
 }
 console.log(generateRandomNumber(-12, 7));
 let generateRandomFloorNumber = function(from, to, afterComma){
+    from = Math.abs(from);
+    to = Math.abs(to);
+    if(from>=to){
+        let tmp = to;
+        to = from;
+        from = tmp;
+    }
     return (from + Math.random() * (to + 1 - from)).toFixed(afterComma);
 }
 generateRandomFloorNumber(18, 11, 3);
