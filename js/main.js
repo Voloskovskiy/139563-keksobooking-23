c51f15c0c2c4af41a7335c42a6dca4f3ca85b1a9
Artem Voloskov <artem@voloskov.ru>
2021-06-29 10:43:29 +0300

    added mentor requirements to the work of random functions

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
-//Первоисточник
 let generateRandomNumber = function(from, to){
-    from = Math.abs(from);
-    to = Math.abs(to);
-    if(from>=to){
-        let tmp = to;
-        to = from;
-        from = tmp;
-    }
-    return Math.floor(from + Math.random() * (to + 1 - from));
+  from = Math.abs(from);
+  to = Math.abs(to);
+  if (from>=to) {
+    const tmp = to;
+    to = from;
+    from = tmp;
+  }
+  return Math.floor(Math.random() * (to - from + 1) ) + from;
 }
-console.log(generateRandomNumber(-12, 7));
+generateRandomNumber(-12, 7);
 let generateRandomFloorNumber = function(from, to, afterComma){
-    from = Math.abs(from);
-    to = Math.abs(to);
-    if(from>=to){
-        let tmp = to;
-        to = from;
-        from = tmp;
-    }
-    return (from + Math.random() * (to + 1 - from)).toFixed(afterComma);
+  from = Math.abs(from)*(10*afterComma);
+  to = Math.abs(to)*(10*afterComma);
+  if (from>=to) {
+    const tmp = to;
+    to = from;
+    from = tmp;
+  }
+  return +((Math.floor(Math.random() * (to - from + 1) ) + from)/(10*afterComma)).toFixed(afterComma);
 }
 generateRandomFloorNumber(18, 11, 3);
